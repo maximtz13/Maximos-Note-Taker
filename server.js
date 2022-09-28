54d07d609f7a02f6dcf8986fba3c181374794b23
Maximoo <maximtz99@gmail.com>
2022-09-28 08:22:47 -0500

    added index.js to html routes, edited server .js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require("./routes/apiRoutes/index")(app);
-require("./routes/htmlRoutes/index")(app);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on http://localhost:${PORT} !`);
