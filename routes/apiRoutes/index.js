c72e747d06f9ff7a3ab5aa231615c61f5cddd41d
Maximoo <maximtz99@gmail.com>
2022-09-28 08:19:43 -0500

    removed testing until a future update, added index.js to apiroutes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { createNewNote, readNotes, deleteNote } = require('../../lib/notes');
+const uniqid = require('uniqid');
+
+router.get('/notes', (req, res) => {
+    res.json(readNotes());
+});
+
+router.post('/notes', (req, res) => {
+    if (!req.body.title) {
+        res.status(400).send('The note did not contain a title.');
+    } else {
+        req.body.id = uniqid();
+        createNewNote(req.body);
+        res.json(req.body);
+    }
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const updatedNotes = deleteNote(req.params.id);
+    res.json(updatedNotes);
+});
+
+module.exports = router;
