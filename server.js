cd96339557366364bac896d527c01fa5fcb015db
Maximoo <maximtz99@gmail.com>
2022-09-28 02:08:20 -0500

    server.js set up!

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// require("./routes/api-routes")(app);
+// require("./routes/html-routes")(app);
+
+app.listen(PORT, () => {
+    console.log(`API server now on http://localhost:${PORT} !`);
+});
